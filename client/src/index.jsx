203a965235017fc8ebb5345ec2b8610d6670c3b1
Brian Tansy <btansy@gmail.com>
2020-01-29 14:19:15 -0800

    cleaned up

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-console.log('index loaded');
-
 ReactDOM.render(<App/>, document.getElementById('app'));
