5f0b90f3f71a83731b843c1e1886cbca423fb644
Brian Tansy <btansy@gmail.com>
2020-01-29 13:00:46 -0800

    webpack configured

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,21 @@ var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
+  mode: 'development',
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR
   },
-  module : {
-    loaders : [
+  module: {
+    rules: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
-        query: {
-          presets: ['react', 'es2015']
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      }
+      } 
     ]
   }
 };
