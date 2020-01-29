669b9ca21daca6f785811669c2407772edb6306a
Brian Tansy <btansy@gmail.com>
2020-01-29 14:06:25 -0800

    react app rendering on page

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,7 @@
-var a = 'test';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+console.log('index loaded');
+
+ReactDOM.render(<App/>, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
 app.use(express.static('public'));
+app.use(express.static('client/dist'));
+// app.use(express.static(path.join(__dirname, '../client', 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html')
+  res.sendFile('index.html');
 });
+
+// app.get('/bundle.js', (req, res) => {
+//   res.sendFile('bundle.js');
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
