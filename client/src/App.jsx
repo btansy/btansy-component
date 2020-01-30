e64015948e8e98cc5f9890774f4fece2df191265
Brian Tansy <btansy@gmail.com>
2020-01-29 17:23:09 -0800

    now with image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
   render() {
     return (
       <div>
-        Hello World
+        <img src="https://target.scene7.com/is/image/Target/GUEST_8520431c-e57f-4806-80a5-50576bdb0cff?wid=700&amp;hei=700&amp;qlt=80&amp;fmt=webp" alt=""></img>
       </div>
     );
   }
