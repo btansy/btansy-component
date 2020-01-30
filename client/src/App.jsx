5fe2c57a8c1dc12e6fc1281378dda635e521b1d5
Brian Tansy <btansy@gmail.com>
2020-01-30 10:26:30 -0800

    shoe pictures

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,8 +16,8 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <img src="https://target.scene7.com/is/image/Target/GUEST_8520431c-e57f-4806-80a5-50576bdb0cff?wid=700&amp;hei=700&amp;qlt=80&amp;fmt=webp" alt=""></img>
+      <div className=''>
+        <img src="https://target.scene7.com/is/image/Target/GUEST_a78a5f7b-a789-4ace-89c1-6530472a0f09?wid=700&amp;hei=700&amp;qlt=80&amp;fmt=webp" alt=""></img>
       </div>
     );
   }
