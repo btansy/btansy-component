c6218fba83fe0b0c569a1ecb58dad6dcadbb911e
Brian Tansy <btansy@gmail.com>
2020-01-30 18:17:00 -0800

    need to figure out aspect ratio

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+
+    };
   }
 
   componentDidMount() {
@@ -16,13 +18,27 @@ class App extends React.Component {
 
   render() {
     return (
+      <div id='size'>
       <div className="container">
-        <div className="item one">Image 1</div>
-        <div className="item two">Image 2</div>
-        <div className="item three">Image 3</div>  
-        <div className="item four">Image 4</div>
-        <div className="item five">More</div>
-        <div className="item display">Image Big</div>
+        <div className="item one">
+          <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt=""></img>
+        </div>
+        <div className="item two">
+          <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt=""></img>
+        </div>
+        <div className="item three">
+          <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt=""></img>
+        </div>  
+        <div className="item four">
+        <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt=""></img>
+        </div>
+        <div className="item five">
+        <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt=""></img>
+        </div>
+        <div className="item display">
+        <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt=""></img>
+        </div>
+      </div>
       </div>
     );
   }
