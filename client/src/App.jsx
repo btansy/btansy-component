1f66cffda8b158d630eded77f0004b11c2b10853
Brian Tansy <btansy@gmail.com>
2020-02-02 12:14:09 -0800

    sidebar now updates the main image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.sideBarClick = this.sideBarClick.bind(this);
     this.state = {
-
+      imgMain:'https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?',
+      imgOne:'https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?',
+      imgTwo:'https://target.scene7.com/is/image/Target/GUEST_077a9562-86e2-457a-b7ec-afb1f7d8416c?',
+      imgThree: 'https://target.scene7.com/is/image/Target/GUEST_6ec263bd-0c5b-4de1-a80d-3c0533b0b8e0?',
+      imgFour: 'https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?',
+      imgFive: 'https://d28m5bx785ox17.cloudfront.net/v1/img/h51TdE6kEIKxsaufROdLDDLuk4cK5tyTXutnDXmSlaI=/sc/600x600'
     };
   }
 
@@ -16,51 +22,34 @@ class App extends React.Component {
 
   }
 
+  sideBarClick(newMain) {
+    this.setState({
+      imgMain: newMain
+    });
+  }
+
   render() {
     return (
-        // <div className="container">
-        //   <div className="item one">
-        //     <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt=""></img>
-        //   </div>
-        //   <div className="item two">
-        //     <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt=""></img>
-        //   </div>
-        //   <div className="item three">
-        //     <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt=""></img>
-        //   </div>  
-        //   <div className="item four">
-        //     <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt=""></img>
-        //   </div>
-        //   <div className="item five">
-        //     <p id='more'>More</p>
-        //     <div id='less'>
-        //       <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt=""></img>
-        //     </div>
-        //   </div>
-        //   <div className="item display">
-        //     <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt=""></img>
-        //   </div>
-        // </div>
 <div className="container">
   <figure className="item one">
-    <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt="Gallery image 1" className="image"></img>
+    <img src={this.state.imgOne} onClick={() => this.sideBarClick(this.state.imgOne)}alt="Gallery image 1" className="image"></img>
   </figure>
-  <figure className="item two">
-    <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt="Gallery image 2" className="image"></img>
+  <figure className="item two" onClick={() => this.sideBarClick(this.state.imgTwo)}>
+    <img src={this.state.imgTwo} alt="Gallery image 2" className="image"></img>
   </figure>
-  <figure className="item three">
-    <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt="Gallery image 3" className="image"></img>
+  <figure className="item three" onClick={() => this.sideBarClick(this.state.imgThree)}>
+    <img src={this.state.imgThree} alt="Gallery image 3" className="image"></img>
   </figure>
-  <figure className="item four">
-    <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt="Gallery image 4" className="image"></img>
+  <figure className="item four" onClick={() => this.sideBarClick(this.state.imgFour)}>
+    <img src={this.state.imgFour} alt="Gallery image 4" className="image"></img>
   </figure>
-  <figure className="item five">
-    <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt="Gallery image 5" className="image"></img>
+  <figure className="item five" onClick={() => this.sideBarClick(this.state.imgFive)}>
+    <img src={this.state.imgFive} alt="Gallery image 5" className="image"></img>
   </figure>
   <div className='item five shade'></div>
   <div className='item five text'>+ more</div>
-  <figure className="item six">
-    <img src="https://target.scene7.com/is/image/Target/GUEST_e12c9357-b0b4-4192-861d-b52bb2f05c71?" alt="Gallery image 6" className="image"></img>
+  <figure className="item main">
+    <img src={this.state.imgMain} alt="Gallery image 6" className="image"></img>
   </figure>
 </div>
     );
