4cf21793646a4460a47ddc7714d9bcd6968f9d25
Brian Tansy <btansy@gmail.com>
2020-02-06 11:20:15 -0800

    added cors

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const database = require('../database/index.js');
+var cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
